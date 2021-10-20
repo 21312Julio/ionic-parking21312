1da203e0774801a20d49437665685f8ce4224611
julio <21312@student.dorset-college.ie>
2021-10-20 02:37:13 +0100

    First Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ParkedCar } from '../models/parked-car';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,55 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  runningTotal = 0;
+  minimumCharge = 2;
+
+  listOfCharges: ParkedCar[] = [];
+
+  selectedRegistration: string;
+  selectedHours: number;
+
   constructor() {}
 
+  calculateCharge(): void {
+    if (isNaN(this.selectedHours) || this.selectedHours <= 0) {
+      return;
+    }
+
+    if (this.selectedRegistration.length === 0) {
+      return;
+    }
+
+    if (this.listOfCharges.filter(charge => charge.registration === this.selectedRegistration).length > 0) {
+      return;
+    }
+
+    const currentCharge = this.calculateParkingCharge(this.selectedHours);
+    this.runningTotal += currentCharge;
+    const newCar = {
+      registration: this.selectedRegistration,
+      hoursParked: this.selectedHours,
+      parkingCharge: currentCharge,
+      runningTotal: this.runningTotal
+    };
+
+    this.listOfCharges.push(newCar);
+  }
+
+  calculateParkingCharge(hours: number): number {
+    const partialCharge = .5;
+    if (hours <= 3) {
+      return this.minimumCharge;
+    }
+
+    const totalCharge = ((hours-3)*partialCharge) + this.minimumCharge;
+
+    return totalCharge <= 10 ? totalCharge : 10;
+  }
+
+  resetButton(): void {
+    this.runningTotal = 0;
+    this.listOfCharges = [];
+    this.selectedRegistration = '';
+  }
 }
